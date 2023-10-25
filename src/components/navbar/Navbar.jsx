1590389b494dad91b355fe09fbe619fdbd5d7e73
mahima <pawar.mahima23@gmail.com>
2023-10-25 16:21:27 +0530

    okk

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -127,7 +127,7 @@ function NavBar() {
 
 
             <div className="dropdown">
-              <label className="nav-item ">Home</label>
+              <label className="nav-item ">HomeDecor</label>
               <div className="dropdown-content">
 
                 <div className="nav-item">
@@ -217,7 +217,7 @@ function NavBar() {
 
 
 
-            {/* <div className="navCart">
+            <div className="navCart">
               <NavLink
                 exact
                 to="/cart"
@@ -226,10 +226,11 @@ function NavBar() {
                 onClick={handleClick}
               >
                 Cart
+                <span className="ml-2 text-lg font-medium text-white-700 group-" >({cartItems.length})</span>
               </NavLink>
-            </div> */}
+            </div>
 
-            <div className="ml-4 flow-root lg:ml-6">
+            {/* <div className="ml-4 flow-root lg:ml-6">
               <Link to={'/cart'} className="group -m-2 flex items-center p-2" >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -238,7 +239,7 @@ function NavBar() {
                 <span className="ml-2 text-sm font-medium text-gray-700 group-" >({cartItems.length})</span>
                 <span className="sr-only">items in cart, view bag</span>
               </Link>
-            </div>
+            </div> */}
 
 
 
